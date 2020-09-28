ebcd0410c44c3f5f83b59fea522a3052a68f2d40
AV-NASH <avinash21213434@gmail.com>
2020-09-28 22:40:45 +0530

    Adding welcome message

---
+++ AddressBook.java
@@ -0,0 +1,15 @@
+package addressbookpackage;
+
+public class AddressBook {
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Welcome to the addressbook");
+		
+
+	}
+	
+
+	
+
+}
