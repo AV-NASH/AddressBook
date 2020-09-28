169e4df3576004a3bc8f0d7e24e0673f8893f4a3
AV-NASH <avinash21213434@gmail.com>
2020-09-28 22:42:32 +0530

    Adding class variables like name,address etc

--- AddressBook.java
+++ AddressBook.java
@@ -2,6 +2,16 @@ package addressbookpackage;
 
 public class AddressBook {
 	
+	private String first_name;
+	private String last_name;
+	private String adress;
+	private String city;
+	private String state;
+	private Long zip;
+	private String phone_number;
+	private String email_id;
+
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Welcome to the addressbook");
