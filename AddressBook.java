05ffd4580a5b93498a9939ace8774c1a6396c28d
AV-NASH <avinash21213434@gmail.com>
2020-09-30 23:48:41 +0530

    Adding feature to hold multiple addressbook in program

--- AddressBook.java
+++ AddressBook.java
@@ -2,17 +2,22 @@ package addressbookpackage;
 
 import java.util.ArrayList;
 import java.util.Scanner;
+import java.util.TreeMap;
 
 
 public class AddressBook {
 									
 	
-	private ArrayList<PersonDetails> adrbook= new ArrayList<PersonDetails>();
+	private TreeMap<String,ArrayList<PersonDetails>> adrbook= new TreeMap<String,ArrayList<PersonDetails>>();
 	
-	public void setDetails() {
+	
+	public void addressBookManager(String adrbookname,ArrayList<PersonDetails> per) {
+		adrbook.put(adrbookname, per);
+	}
+	
+	public ArrayList<PersonDetails> setDetails(ArrayList<PersonDetails> newDetails) {
 		
 		Scanner sc=new Scanner(System.in);
-		
 		
 		System.out.println("Enter your first name");
 		String first_name=sc.nextLine();
@@ -34,25 +39,26 @@ public class AddressBook {
 		
 		PersonDetails p=new PersonDetails(first_name, last_name, address, city, state, zip, phone_number,email_id);
 		
-		adrbook.add(p);
+		newDetails.add(p);
 		System.out.println("Person details added successfully");
+		return newDetails;
 		
 	}
 	
 	
-public void editDetails(String name) {
+public ArrayList<PersonDetails> editDetails(ArrayList<PersonDetails> editDetails,String name) {
 		
 		Scanner sc=new Scanner(System.in);
 		String fname;
 		int i=0;
-		for(PersonDetails per:adrbook) {
+		for(PersonDetails per:editDetails) {
 			
 			fname=per.getFirst_name();
 			if(name.equals(fname)) break;
 			i++;
 		}
 		
-		if((i)==adrbook.size()) {
+		if((i)==editDetails.size()) {
 			System.out.println("No record found with such name");
 			
 		}
@@ -78,29 +84,31 @@ public void editDetails(String name) {
 				String email_id=sc.nextLine();
 				
 				PersonDetails newPerson=new PersonDetails(first_name, last_name, address, city, state, zip, phone_number, email_id);
-				adrbook.remove(i);
-				adrbook.add(i, newPerson);
+				editDetails.remove(i);
+				editDetails.add(i, newPerson);
 				
 			System.out.println("Person details edited successfully");
+			
 		
 		}
+		return editDetails;
 		
 	}
+ 
 
-
-public void removeRecord(String name) {
+public ArrayList<PersonDetails> removeRecord(ArrayList<PersonDetails> removeDetails,String name) {
 	
 	Scanner sc=new Scanner(System.in);
 	String fname;
 	int i=0;
-	for(PersonDetails per:adrbook) {
+	for(PersonDetails per:removeDetails) {
 		
 		fname=per.getFirst_name();
-		if(name.equals(fname)) break;
+		if(name.equals(fname)) {break;}
 		i++;
 	}
 	
-	if((i)==adrbook.size()) {
+	if((i)==removeDetails.size()) {
 		System.out.println("No record found with such name");
 		
 	}
@@ -108,12 +116,13 @@ public void removeRecord(String name) {
 	else {
 			
 			
-			adrbook.remove(i);
+			removeDetails.remove(i);
 			
 			
 		System.out.println("Person details removed successfully");
+		
 		}
-	
+	return removeDetails;
 }
 
 	public static void main(String[] args) {
@@ -135,42 +144,56 @@ public  void UserInterfaceFunction() {
 		
 		Scanner sc=new Scanner(System.in);
 		
+		ArrayList<PersonDetails> per=new ArrayList<PersonDetails>();
 		AddressBook AddressBookRecord = new AddressBook();
 		int choice;
-		String name;
+		String name,adrbookname;
+		do {
+		System.out.println("please enter name of the address book ");
+		adrbookname=sc.nextLine();
+		
 		
 		do {
 		System.out.println("1.Add a entry to addressbook");
 		System.out.println("2.Edit a entry of addressbook");
 		System.out.println("3. Delete a entry of addressbook");
-		System.out.println("4. Exit");
+		System.out.println("4. Exit the address book");
 		System.out.println("Enter your choice"); 
 		 choice=sc.nextInt();
 		 sc.nextLine();
 		
 		switch (choice) {
 		case 1: {
-			AddressBookRecord.setDetails();
+			per=AddressBookRecord.setDetails(per);
 			break;
 		}
 		
 		case 2: {
 			System.out.println("Enter first name of person whose record is to be edited");
 			name=sc.nextLine();
-			AddressBookRecord.editDetails(name);
+			per=AddressBookRecord.editDetails(per,name);
 			break;
 		}
 		
 		case 3: {
 			System.out.println("Enter first name of person whose record is to be remove");
 			name=sc.nextLine();
-			AddressBookRecord.removeRecord(name);
+			per=AddressBookRecord.removeRecord(per,name);
 			break;
 		}
 		
 		}
 		
 		}while(choice!=4);
+		
+		AddressBookRecord.addressBookManager(adrbookname, per);
+		
+		System.out.println("1.Create a new addressbook");
+		System.out.println("2.Exit the program");
+		System.out.println("Enter the choice");
+		choice=sc.nextInt();
+		sc.nextLine();
+		} while(choice!=2);
 		
 	}
 }
