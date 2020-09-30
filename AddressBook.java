e1015c83db9319a6c404a093d87dff22b7806b40
AV-NASH <avinash21213434@gmail.com>
2020-09-30 18:06:26 +0530

    Adding feature to hold multiple contacts in addressbook

--- AddressBook.java
+++ AddressBook.java
@@ -1,91 +1,21 @@
 package addressbookpackage;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
+
 public class AddressBook {
+									
 	
-	private String first_name;
-	private String last_name;
-	private String address;
-	private String city;
-	private String state;
-	private Long zip;
-	private String phone_number;
-	private String email_id;
-	
-	
-	public String getFirst_name() {
-		return first_name;
-	}
-
-	public void setFirst_name(String first_name) {
-		this.first_name = first_name;
-	}
-
-	public String getLast_name() {
-		return last_name;
-	}
-
-	public void setLast_name(String last_name) {
-		this.last_name = last_name;
-	}
-
-	public String getAddress() {
-		return address;
-	}
-
-	public void setAddress(String address) {
-		this.address = address;
-	}
-
-	public String getCity() {
-		return city;
-	}
-
-	public void setCity(String city) {
-		this.city = city;
-	}
-
-	public String getState() {
-		return state;
-	}
-
-	public void setState(String state) {
-		this.state = state;
-	}
-
-	public Long getZip() {
-		return zip;
-	}
-
-	public void setZip(Long zip) {
-		this.zip = zip;
-	}
-
-	public String getPhone_number() {
-		return phone_number;
-	}
-
-	public void setPhone_number(String phone_number) {
-		this.phone_number = phone_number;
-	}
-
-	public String getEmail_id() {
-		return email_id;
-	}
-
-	public void setEmail_id(String email_id) {
-		this.email_id = email_id;
-	}
-
-	
+	private ArrayList<PersonDetails> adrbook= new ArrayList<PersonDetails>();
 	
 	public void setDetails() {
 		
 		Scanner sc=new Scanner(System.in);
 		
+		
 		System.out.println("Enter your first name");
-		first_name=sc.nextLine();
+		String first_name=sc.nextLine();
 		System.out.println("Enter your last name");
 		String last_name=sc.nextLine();
 		System.out.println("Enter your address/block number/street name ");
@@ -102,52 +32,89 @@ public class AddressBook {
 		System.out.println("Enter your email_id");
 		String email_id=sc.nextLine();
 		
+		PersonDetails p=new PersonDetails(first_name, last_name, address, city, state, zip, phone_number,email_id);
 		
-		setAddress(address);
-		setCity(city);
-		setEmail_id(email_id);
-		setFirst_name(first_name);
-		setLast_name(last_name);
-		setPhone_number(phone_number);
-		setState(state);
-		setZip(zip);
+		adrbook.add(p);
+		System.out.println("Person details added successfully");
 		
 	}
 	
 	
-	public void editDetails() {
+public void editDetails(String name) {
 		
-Scanner sc=new Scanner(System.in);
+		Scanner sc=new Scanner(System.in);
+		String fname;
+		int i=0;
+		for(PersonDetails per:adrbook) {
+			
+			fname=per.getFirst_name();
+			if(name.equals(fname)) break;
+			i++;
+		}
 		
-		System.out.println("Edit the first name");
-		first_name=sc.nextLine();
-		System.out.println("Edit the last name");
-		String last_name=sc.nextLine();
-		System.out.println("Edit the address/block number/street name ");
-		String address=sc.nextLine();
-		System.out.println("Edit the city");
-		String city=sc.nextLine();
-		System.out.println("Edit the state");
-		String state=sc.nextLine();
-		System.out.println("Edit the zip/pincode");
-		Long zip=sc.nextLong();
-		sc.nextLine();
-		System.out.println("Edit the phone number");
-		String phone_number=sc.nextLine();
-		System.out.println("Edit the email_id");
-		String email_id=sc.nextLine();
+		if((i)==adrbook.size()) {
+			System.out.println("No record found with such name");
+			
+		}
 		
+		else {
+				
+				System.out.println("Edit the first name");
+				String first_name=sc.nextLine();
+				System.out.println("Edit the last name");
+				String last_name=sc.nextLine();
+				System.out.println("Edit the address/block number/street name ");
+				String address=sc.nextLine();
+				System.out.println("Edit the city");
+				String city=sc.nextLine();
+				System.out.println("Edit the state");
+				String state=sc.nextLine();
+				System.out.println("Edit the zip/pincode");
+				Long zip=sc.nextLong();
+				sc.nextLine();
+				System.out.println("Edit the phone number");
+				String phone_number=sc.nextLine();
+				System.out.println("Edit the email_id");
+				String email_id=sc.nextLine();
+				
+				PersonDetails newPerson=new PersonDetails(first_name, last_name, address, city, state, zip, phone_number, email_id);
+				adrbook.remove(i);
+				adrbook.add(i, newPerson);
+				
+			System.out.println("Person details edited successfully");
 		
-		setAddress(address);
-		setCity(city);
-		setEmail_id(email_id);
-		setFirst_name(first_name);
-		setLast_name(last_name);
-		setPhone_number(phone_number);
-		setState(state);
-		setZip(zip);
+		}
 		
 	}
+
+
+public void removeRecord(String name) {
+	
+	Scanner sc=new Scanner(System.in);
+	String fname;
+	int i=0;
+	for(PersonDetails per:adrbook) {
+		
+		fname=per.getFirst_name();
+		if(name.equals(fname)) break;
+		i++;
+	}
+	
+	if((i)==adrbook.size()) {
+		System.out.println("No record found with such name");
+		
+	}
+	
+	else {
+			
+			
+			adrbook.remove(i);
+			
+			
+		System.out.println("Person details removed successfully");
+		}
+	
+}
 
 	public static void main(String[] args) {
 	
@@ -156,11 +123,11 @@ Scanner sc=new Scanner(System.in);
 		UserInterface UIntFace = new UserInterface();
 		UIntFace.UserInterfaceFunction();		
 		
-
 	}
 	
 
 }
+
 
 class UserInterface {
 	
@@ -170,6 +137,7 @@ public  void UserInterfaceFunction() {
 		
 		AddressBook AddressBookRecord = new AddressBook();
 		int choice;
+		String name;
 		
 		do {
 		System.out.println("1.Add a entry to addressbook");
@@ -178,6 +146,7 @@ public  void UserInterfaceFunction() {
 		System.out.println("4. Exit");
 		System.out.println("Enter your choice"); 
 		 choice=sc.nextInt();
+		 sc.nextLine();
 		
 		switch (choice) {
 		case 1: {
@@ -186,12 +155,16 @@ public  void UserInterfaceFunction() {
 		}
 		
 		case 2: {
-			AddressBookRecord.editDetails();
+			System.out.println("Enter first name of person whose record is to be edited");
+			name=sc.nextLine();
+			AddressBookRecord.editDetails(name);
 			break;
 		}
 		
 		case 3: {
-			AddressBookRecord=null;
+			System.out.println("Enter first name of person whose record is to be remove");
+			name=sc.nextLine();
+			AddressBookRecord.removeRecord(name);
 			break;
 		}
 		
